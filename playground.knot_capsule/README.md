a01121e74da511cc3a71dcc1767debd298ce0600
JamieDawson <jamierobertdawson@gmail.com>
2019-09-12 20:25:25 -0700

    basic files working

---
+++ playground.knot_capsule/README.md
@@ -0,0 +1,1 @@
+# playground.knot_capsule
---
+++ playground.knot_capsule/code/show_knots.js
@@ -0,0 +1,20 @@
+module.exports.function = function show_knots () {
+  return [
+    {
+      name_of_knot: "testing this 1",
+      click_text_here: "clicked 1"
+    },
+    {
+      name_of_knot: "testing this 2",
+      click_text_here: "clicked 2"
+    },
+    {
+      name_of_knot: "Testing this 3",
+      click_text_here: "clicked 3"
+    }
+  ]
+
+
+
+
+}
