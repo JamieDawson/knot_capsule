e73969e8f40233f6b8bc07fd5543c740d31061df
Jamie Dawson <jadawson@e1z2r5p35.42.us.org>
2019-09-17 18:37:37 -0700

    still trying to get views to work" " "

--- playground.knot_capsule/code/show_knots.js
+++ playground.knot_capsule/code/show_knots.js
@@ -2,19 +2,18 @@ module.exports.function = function show_knots () {
   return [
     {
       name_of_knot: "testing this 1",
-      click_text_here: "clicked 1"
+      click_text_here: "clicked 1",
+      image_URL: 'images/bowline_1.jpg'
     },
     {
       name_of_knot: "testing this 2",
-      click_text_here: "clicked 2"
+      click_text_here: "clicked 2",
+
     },
     {
       name_of_knot: "Testing this 3",
-      click_text_here: "clicked 3"
+      click_text_here: "clicked 3",
+
     }
   ]
-
-
-
-
 }
