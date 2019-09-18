343f9d703f26ec952a94c4713a41206850ebc8a2
Jamie Dawson <jadawson@e1z2r4p25.42.us.org>
2019-09-18 16:51:05 -0700

    can handle images. Now needs ot handle text

--- playground.knot_capsule/code/show_knots.js
+++ playground.knot_capsule/code/show_knots.js
@@ -3,13 +3,14 @@ module.exports.function = function show_knots () {
     {
       name_of_knot: "testing this 1",
       click_text_here: "clicked 1",
-      image_URL: 'images/bowline_7.jpg', //last image for summary pic
-      images: [{url:'/images/bowline_1.jpg'}, {url:"/images/bowline_2.png"}, {url:"/images/bowline_3"}, {url:"/images/bowline_4"}, {url:"/images/bowline_5"}, {url:"/images/bowline_6"}, {url:"/images/bowline_7"}],
+      main_image: 'images/bowline_7.jpg', //last image for summary pic
+      mult_pic: [{url:'/images/bowline_5.jpg'}, {url:'/images/bowline_6.jpg'}],
+      mult_des: [{url:'says the first words'}, {url:'Saying the second words'}]
     },
     {
       name_of_knot: "testing this 2",
       click_text_here: "clicked 2",
-      image_URL: 'images/bowline_1.jpg',
+      main_image: 'images/bowline_1.jpg',
     },
     {
       name_of_knot: "Testing this 3",
