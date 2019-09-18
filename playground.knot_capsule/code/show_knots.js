b39e499fc81700e2f51a6149ec2ef975566f8bce
Jamie Dawson <jadawson@e1z2r5p35.42.us.org>
2019-09-17 19:01:14 -0700

    fixed js and views issue. Now I just gotta add the content

--- playground.knot_capsule/code/show_knots.js
+++ playground.knot_capsule/code/show_knots.js
@@ -3,17 +3,17 @@ module.exports.function = function show_knots () {
     {
       name_of_knot: "testing this 1",
       click_text_here: "clicked 1",
-      image_URL: 'images/bowline_1.jpg'
+      image_URL: 'images/bowline_1.jpg',
     },
     {
       name_of_knot: "testing this 2",
       click_text_here: "clicked 2",
-
+      image_URL: 'images/bowline_1.jpg',
     },
     {
       name_of_knot: "Testing this 3",
       click_text_here: "clicked 3",
-
+     // image_URL: 'images/bowline_1.jpg',
     }
   ]
 }
