7e1355a0cd8c0bc357b865ab396703bc956a1910
Jamie Dawson <jadawson@e1z2r4p25.42.us.org>
2019-09-18 15:42:14 -0700

    views issue

--- playground.knot_capsule/code/show_knots.js
+++ playground.knot_capsule/code/show_knots.js
@@ -5,7 +5,6 @@ module.exports.function = function show_knots () {
       click_text_here: "clicked 1",
       image_URL: 'images/bowline_7.jpg', //last image for summary pic
       images: [{url:'/images/bowline_1.jpg'}, {url:"/images/bowline_2.png"}, {url:"/images/bowline_3"}, {url:"/images/bowline_4"}, {url:"/images/bowline_5"}, {url:"/images/bowline_6"}, {url:"/images/bowline_7"}],
-    //images: ["/images/bowline_1.jpg", "/images/bowline_2.png", "/images/bowline_3", "/images/bowline_4", "/images/bowline_5", "/images/bowline_6", "/images/bowline_7"],
     },
     {
       name_of_knot: "testing this 2",
@@ -15,7 +14,6 @@ module.exports.function = function show_knots () {
     {
       name_of_knot: "Testing this 3",
       click_text_here: "clicked 3",
-     // image_URL: 'images/bowline_1.jpg',
     }
   ]
 }
