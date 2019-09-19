b2f81efb481bdf2c120b98146c79a8a8782fb431
Jamie Dawson <jadawson@e1z2r4p25.42.us.org>
2019-09-18 18:19:11 -0700

    update bowline knot

--- playground.knot_capsule/code/show_knots.js
+++ playground.knot_capsule/code/show_knots.js
@@ -1,20 +1,17 @@
 module.exports.function = function show_knots () {
   return [
     {
-      name_of_knot: "testing this 1",
-      click_text_here: "clicked 1",
+      name_of_knot: "Bowline Knot",
       main_image: 'images/bowline_7.jpg', //last image for summary pic
-      mult_pic: [{url:'/images/bowline_5.jpg', text:'yes here'}, {url:'/images/bowline_6.jpg'}],
-      mult_des: [{url:"www.url.com", text:'says the first words'}, {url:"www.url.com/2", text:'Saying the second words'}]
+      mult_pic: [{url:'/images/bowline_1.jpg', text:'Lay out your rope.'}, {url:'/images/bowline_2.jpg', text:'Make a small loop.'},{url:'/images/bowline_4.jpg', text:'Pass the end of the rope through the loop you created.'},{url:'/images/bowline_5.jpg', text:'Place the end of the rope behind the standing end.'},{url:'/images/bowline_6.jpg', text:'Pass around the standing end'},{url:'/images/bowline_7.jpg', text:'Place the end inside the loop.'}],
+      //mult_des: [{url:"www.url.com", text:'says the first words'}, {url:"www.url.com/2", text:'Saying the second words'}]
     },
     {
       name_of_knot: "testing this 2",
-      click_text_here: "clicked 2",
       main_image: 'images/bowline_1.jpg',
     },
     {
       name_of_knot: "Testing this 3",
-      click_text_here: "clicked 3",
     }
   ]
 }
