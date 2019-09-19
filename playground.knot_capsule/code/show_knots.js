7aa498cceea65958db3b8eb9751d21d83646e618
Jamie Dawson <jadawson@e1z2r4p25.42.us.org>
2019-09-18 17:38:38 -0700

    views now work

--- playground.knot_capsule/code/show_knots.js
+++ playground.knot_capsule/code/show_knots.js
@@ -4,7 +4,7 @@ module.exports.function = function show_knots () {
       name_of_knot: "testing this 1",
       click_text_here: "clicked 1",
       main_image: 'images/bowline_7.jpg', //last image for summary pic
-      mult_pic: [{url:'/images/bowline_5.jpg'}, {url:'/images/bowline_6.jpg'}],
+      mult_pic: [{url:'/images/bowline_5.jpg', text:'yes here'}, {url:'/images/bowline_6.jpg'}],
       mult_des: [{url:"www.url.com", text:'says the first words'}, {url:"www.url.com/2", text:'Saying the second words'}]
     },
     {
